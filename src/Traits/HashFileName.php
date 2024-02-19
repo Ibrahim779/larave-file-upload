51f7b10588d6862c662caf470a33ad1aa14a252a
Ibrahim Ismail <55800801+Ibrahim779@users.noreply.github.com>
2024-02-19 09:01:01 +0200

    Create HashFileName.php

---
+++ src/Traits/HashFileName.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Services\FileUpload\Trait;
+
+use Exception;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+trait HashFileName
+{
+
+    /**
+     * Return Hashed File Name.
+     *
+     * @access protected
+     * @return string
+     */
+    protected function hashFileName()
+    {
+        return hash('md5', rand(100, 999)) . Str::random(10);
+    }
+
+}
